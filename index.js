867356a1fe646d6ea1938e7ad33924f5a13240be
Ananya Agrawal <ananyaagrawal2210@gmail.com>
2025-04-22 05:07:44 +0530

    Add GitHub Actions workflow

--- index.js
+++ index.js
@@ -10,10 +10,22 @@
 // app.listen(port, () => {
 //   console.log(`Server running on http://localhost:${port}`);
 // });
+
+// // Latest Working Code 1.1
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => res.send("Hello from version 1.0.0"));
+
+// app.listen(port, () => console.log(`App running on port ${port}`));
+
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello from version 1.0.0"));
+// Introduce a syntax error to simulate failure (missing parentheses)
+app.get("/", (req, res) => res.send("Hello from version 1.2";)  // Error here
 
 app.listen(port, () => console.log(`App running on port ${port}`));
