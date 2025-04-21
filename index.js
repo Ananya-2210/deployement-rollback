904c9632ecc57bb228f62228e200d8457263622b
Ananya Agrawal <ananyaagrawal2210@gmail.com>
2025-04-22 04:26:51 +0530

    .

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from version 1.0.0</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
